7c0520de179c03d3255b49f3bfb64f742d45e66e
Ayush Raj <ayushraj7006@gmail.com>
2026-09-17 16:47:56 +0530

    Time: 2 ms (94.25%), Space: 45.8 MB (50.67%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,26 @@
 class Solution {
-    private void combinations(int[] candidates, int target, int i, int sum, List<Integer> current, List<List<Integer>> result) {
-        if (i == candidates.length || sum > target) {
+    private void combinations(int[] candidates, int target, int i, List<Integer> current, List<List<Integer>> result) {
+        if (i == candidates.length || target < 0) {
             return;
         }
 
-        if (sum == target) {
+        if (target == 0) {
             result.add(new ArrayList<>(current));
             return;
         }
 
         current.add(candidates[i]);
-        combinations(candidates, target, i, sum + candidates[i], current, result);
+        combinations(candidates, target - candidates[i], i, current, result);
 
         current.remove(current.size() - 1);
-        combinations(candidates, target, i + 1, sum, current, result);
+        combinations(candidates, target, i + 1, current, result);
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
 
-        combinations(candidates, target, 0, 0, current, result);
+        combinations(candidates, target, 0, current, result);
 
         return result;
     }
