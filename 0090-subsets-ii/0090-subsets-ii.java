8cafc67c306a62aff93f713baf5fbd307e923ca2
Ayush Raj <ayushraj7006@gmail.com>
2026-09-17 22:27:42 +0530

    Time: 3 ms (53.56%), Space: 45.2 MB (23.69%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,28 +1,24 @@
 class Solution {
-    private void subsets(int[] nums, int i, List<Integer> current, List<List<Integer>> result, Set<List<Integer>> seen) {
-        if (i == nums.length) {
-            if (!seen.contains(current)) {
-                seen.add(new ArrayList<>(current));
-                result.add(new ArrayList<>(current));
-            }
-            return;
+    private void subsets(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) continue;
+
+            current.add(nums[i]);
+            subsets(nums, i + 1, current, result);
+
+            current.remove(current.size() - 1);
         }
-
-        current.add(nums[i]);
-        subsets(nums, i + 1, current, result, seen);
-
-        current.remove(current.size() - 1);
-        subsets(nums, i + 1, current, result, seen);
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> current = new ArrayList<>();
-        Set<List<Integer>> seen = new HashSet<>();
-        
+
         Arrays.sort(nums);
 
-        subsets(nums, 0, current, result, seen);
+        subsets(nums, 0, current, result);
         return result;
     }
 }
